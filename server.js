3930d27b22950010ad5608b8d931e445572034e4
Jonathan Roy <jonroy7@gmail.com>
2018-05-06 02:23:25 +0200

    Turn off ELF stats for now, doing table scans.

--- server.js
+++ server.js
@@ -967,14 +967,15 @@ app.get('/',  asyncMiddleware( async (req, res, next) => {
         .then((list) => {
             return (list.length + " in past hour.<br>");
         }),
-        db.collection("games").find({ _id: { $gt: objectIdFromDate(Date.now() - 1000 * 60 * 60 * 24) }, networkhash: { $ne : ELF_NETWORK } }).count()
+        db.collection("games").find({ _id: { $gt: objectIdFromDate(Date.now() - 1000 * 60 * 60 * 24) } }).count()
         .then((count) => {
-            return (counter + " total LZ selfplay games, (" + count + " in past 24 hours, ");
+            return (counter + " total selfplay games, (" + count + " in past 24 hours, ");
         }),
-        db.collection("games").find({ _id: { $gt: objectIdFromDate(Date.now() - 1000 * 60 * 60) }, networkhash: { $ne: ELF_NETWORK } }).count()
+        db.collection("games").find({ _id: { $gt: objectIdFromDate(Date.now() - 1000 * 60 * 60) } }).count()
         .then((count) => {
             return (count + " in past hour).<br/>");
         }),
+/*
         db.collection("games").find({ _id: { $gt: objectIdFromDate(Date.now() - 1000 * 60 * 60 * 24) }, networkhash: ELF_NETWORK }).count()
         .then((count) => {
             return (elf_counter + " total ELF selfplay games, (" + count + " in past 24 hours, ");
@@ -983,6 +984,7 @@ app.get('/',  asyncMiddleware( async (req, res, next) => {
         .then((count) => {
             return (count + " in past hour).<br/>");
         }),
+*/
         db.collection("match_games").find().count()
         .then((count) => {
             return (count + " total match games. (");
