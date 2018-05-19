e4e59b51e810d45dd4587cbd30504d0785a454a4
Ed Lee <edilee@mozilla.com>
2018-05-19 15:23:23 -0700

    Add eslint config file with recommended except no-console

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "rules": {
+        "no-console": 0
+    }
+};
