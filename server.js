a62957c4932620036e176085259b8385edb1ef48
Bood Qian <bood@glowing.com>
2018-06-06 15:30:27 +0800

    Access Log: Log into `logs` collection of mongodb
    Also install npm@5.6 locally for the project to avoid switching versions

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const discord = require("./classes/discord");
 const morgan = require("morgan");
 const rfs = require("rotating-file-stream");
 const dbutils = require("./classes/dbutils");
+const mongoMorgan = require("mongo-morgan");
+
+const MONGODB_URL = "mongodb://localhost/test";
 
 /**
  * Request Logging
@@ -43,6 +46,15 @@ morgan.token("memory", () => {
 
     return usage.join(", ");
 });
+
+// Save access log to `logs` collection
+app.use(
+    mongoMorgan(
+        MONGODB_URL,
+        "{\"method\": \":method\", \"url\": \":url\", \"status\": :status, \"response-time\": :response-time}",
+        { collection: "logs" })
+    );
+
 app.use(morgan("-->Before :memory", { stream: logStream, immediate: true }));
 app.use(morgan(":method :url :status :req[content-length] :response-time ms", { stream: logStream, immediate: false }));
 app.use(morgan("-->After  :memory", { stream: logStream, immediate: false }));
@@ -300,7 +312,7 @@ setInterval(() => {
     }
 }, 1000 * 60 * 1);
 
-MongoClient.connect("mongodb://localhost/test", (err, database) => {
+MongoClient.connect(MONGODB_URL, (err, database) => {
     if (err) return console.log(err);
 
     db = database;
